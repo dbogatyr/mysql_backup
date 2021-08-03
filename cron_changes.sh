e1ac06e114a19c024abce6f7b672999e21a299c3
dbogatyr <82878044+dbogatyr@users.noreply.github.com>
2021-08-03 13:44:22 +0300

    Update cron_changes.sh

--- cron_changes.sh
+++ cron_changes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# EX: sudo ./cron_changes.sh dbogatyr 
+# EX: sudo ./cron_changes.sh dbogatyr
 
 USERNAME=$1
-BACKUP_PATH=/home/$USERNAME/backup_mysql
+BACKUP_PATH=/tmp
 # Backup current tasks
 crontab -l -u $USERNAME > $USERNAME.date.cron
 # Remove existing tasks
@@ -13,3 +13,5 @@ echo $USERNAME >> /etc/cron.allow
 touch /var/spool/cron/$USERNAME
 /usr/bin/crontab /var/spool/cron/$USERNAME
 echo "* * * * * /bin/bash $BACKUP_PATH/dump.sh" >> /var/spool/cron/$USERNAME
+chown $USERNAME:$USERNAME /var/spool/cron/$USERNAME
+chmod 600 /var/spool/cron/$USERNAME
